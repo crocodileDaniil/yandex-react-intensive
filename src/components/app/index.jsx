9742b6143c0dfe1fd45a0f0f6819ff945f795eb9
CrocodileDaniil <karasev_dan_an@mail.ru>
2025-03-24 00:24:41 +0300

    step-2

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,8 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { PageConstructorBurger } from '../pages/page-constructor-burger/page-constructor-burger';
 import { CheckList } from '../pages/check-list/check-list';
 import { MOCK_DATA } from '../../constants/mock';
 import { PersonalAccount } from '../pages/persanal-account/personal-account';
+import { url } from '@utils/url';
+import { CometLoader } from '../loader/comet-loader';
+import { Error } from '../error/error';
 
 //почему этот импорт не работает?
 // import { MOCK_DATA } from '@constants/mock';
@@ -11,6 +14,43 @@ const restaurantPages = ['constructor', 'check-list', 'personal-account'];
 
 export const App = () => {
 	const [page, setPage] = useState(restaurantPages[0]);
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState({
+		hasError: false,
+		errorMessage: '',
+	});
+
+	useEffect(() => {
+		const getIngredients = async () => {
+			try {
+				const response = await fetch(url);
+				if (!response.ok) {
+					setLoading(false);
+					setError({
+						hasError: true,
+						errorMessage: 'Error loading data',
+					});
+					throw new Error('Error loading data');
+				}
+				const data = await response.json();
+				setData(data.data);
+				setLoading(false);
+				setError({
+					...error,
+					hasError: false,
+				});
+			} catch (err) {
+				setError({
+					hasError: true,
+					errorMessage: err.message,
+				});
+				setLoading(false);
+			}
+		};
+
+		getIngredients();
+	}, []);
 
 	// здесь оборачивать не обязаятельно?
 	const setPageConstructor = () => {
@@ -25,7 +65,9 @@ export const App = () => {
 		setPage(restaurantPages[2]);
 	};
 
-	return (
+	return loading ? (
+		<CometLoader />
+	) : !error.hasError ? (
 		<>
 			{page === restaurantPages[0] ? (
 				<PageConstructorBurger
@@ -33,7 +75,7 @@ export const App = () => {
 					setPageCheckList={setPageCheckList}
 					setPagePersonalAccount={setPagePersonalAccount}
 					activePage={page}
-					data={MOCK_DATA}
+					data={data}
 				/>
 			) : null}
 			{page === restaurantPages[1] ? (
@@ -42,7 +84,7 @@ export const App = () => {
 					setPageCheckList={setPageCheckList}
 					setPagePersonalAccount={setPagePersonalAccount}
 					activePage={page}
-					data={MOCK_DATA}
+					data={data}
 				/>
 			) : null}
 			{page === restaurantPages[2] ? (
@@ -51,9 +93,11 @@ export const App = () => {
 					setPageCheckList={setPageCheckList}
 					setPagePersonalAccount={setPagePersonalAccount}
 					activePage={page}
-					data={MOCK_DATA}
+					data={data}
 				/>
 			) : null}
 		</>
+	) : (
+		<Error text={error.errorMessage} />
 	);
 };
