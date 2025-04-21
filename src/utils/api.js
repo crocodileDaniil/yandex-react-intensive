e087afd20fe36287d9b5c0c55dc0b102fb03bc1b
CrocodileDaniil <karasev_dan_an@mail.ru>
2025-04-22 00:49:41 +0300

    add two api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-import { URL_GET_INGREDIENTS, URL_POST_PLACE_ORDER } from './url';
+import { URL_GET_INGREDIENTS, URL_POST_AUTH_TOKEN, URL_POST_PLACE_ORDER } from './url';
 
 const getResponse = async (res) => {
 	if (res.ok) return await res.json();
@@ -40,4 +40,68 @@ export const postPlaceOrderApi = async (body) => {
 	} catch (e) {
 		throw e;
 	}
+};
+
+export const registerUser = async (userData) => {
+	try {
+		const response = await fetch('https://your-api.com/api/register', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8',
+			},
+			body: JSON.stringify(userData),
+		});
+
+		const data = await response.json();
+
+		if (response.ok) {
+			localStorage.setItem('accessToken', data.accessToken);
+			localStorage.setItem('refreshToken', data.refreshToken);
+			console.log('register Completed');
+		} else {
+			console.error('Registration error:', data.message);
+		}
+	} catch (error) {
+		console.error('Request failed:', error);
+	}
+};
+
+export const fetchWithAuth = async (url, options = {}, retry = true) => {
+	let accessToken = localStorage.getItem('accessToken');
+	options.headers = {
+		...(options.headers || {}),
+		Authorization: `Bearer ${accessToken}`,
+		'Content-Type': 'application/json ;charset=utf-8',
+	};
+
+	let response = await fetch(url, options);
+
+	// Если токен истёк
+	if (response.status === 401 && retry) {
+		const refreshToken = localStorage.getItem('refreshToken');
+
+		const tokenResponse = await fetch(
+			URL_POST_AUTH_TOKEN, // переобновление токена
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ refreshToken }),
+			}
+		);
+
+		if (tokenResponse.ok) {
+			const newTokens = await tokenResponse.json();
+			localStorage.setItem('accessToken', newTokens.accessToken);
+			localStorage.setItem('refreshToken', newTokens.refreshToken);
+
+			// Повторяем оригинальный запрос с новым токеном
+			return fetchWithAuth(url, options, false);
+		} else {
+			console.error('Refresh token failed');
+		}
+	}
+
+	return response;
 };
